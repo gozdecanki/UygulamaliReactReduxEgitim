782b32acbf4a2a966b7d257a74dcb5515a7f94b2
gozdecanki <gozde_can.92@hotmail.com>
2025-06-03 22:56:42 +0300

    BurgerYiyelim Projesi

---
+++ BurgerYiyelim/burger/src/components/Contact.js
@@ -0,0 +1,25 @@
+import React from "react";
+import BannerImage from '../assets/banner.png'
+import  '../styles/Contact.css'
+
+export const Contact = () => {
+  return (
+    <div className="contact">
+      <div className="leftSide"  style={{backgroundImage:`url(${BannerImage})`}}></div>
+      <div className="rightSide">
+        <h1>Bizimle iletişime geçin</h1>
+        <form>
+          <label>Ad Soyad</label>
+          <input type="text" name='name' placeholder="Lütfen adınızı soyadınızı giriniz..."/>
+
+          <label>Email</label>
+          <input type="email" name="email" placeholder="Lütfen emailinizi giriniz..."/>
+
+          
+          <label>Mesajınız</label>
+          <textarea rows="6"  name="message" placeholder="Lütfen mesajınızı giriniz..."/>
+        </form>
+      </div>
+    </div>
+  );
+};
