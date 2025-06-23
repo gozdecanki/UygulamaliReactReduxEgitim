32dcfcc31fe5647584eaf59e2673614577ae353e
gozdecanki <gozde_can.92@hotmail.com>
2025-06-23 21:53:41 +0300

    class, access modifiers, inheritance, abstract class ,static , readonly , type intersection , instanceof ,generics, optional field

--- Typescript/1-Kurulum/app.js
+++ Typescript/1-Kurulum/app.js
@@ -1,5 +1,6 @@
-console.log("Typescript e giriş yaptık");
-console.log("Typescript e giriş yaptık -w ile"); // ts dosyasındaki değişikliğin otomatik olarak js ye aktarmak için tsc app.ts -w ile komut satırını çalıştırıyoruz
+/* console.log("Typescript e giriş yaptık");
+console.log("Typescript e giriş yaptık -w ile");// ts dosyasındaki değişikliğin otomatik olarak js ye aktarmak için tsc app.ts -w ile komut satırını çalıştırıyoruz
+ */
 //data annotation
 /* let age:number=29;
 let firstname:string="Gözde";
@@ -440,18 +441,557 @@ console.log(degisken2);
 }
 birlestir("Merhaba","Gözde","Can","Osman"); */
 //class 
-var Person = /** @class */ (function () {
-    function Person(id, firstName, lastName) {
-        this.id = id,
-            this.firstName = firstName,
-            this.lastName = lastName;
-    }
-    Person.prototype.getFullName = function () {
-        //return this.firstName +' '+this.lastName;
-        return "".concat(this.firstName, " ").concat(this.lastName);
-    };
-    return Person;
-}());
-var kisiBilgim = new Person(1, "Gözde", "Cankı");
+/*class Person {
+
+  // id;
+  // firstName;
+  // lastName;
+  //  constructor(id, firstName, lastName){
+  //     this.id = id,
+  //     this.firstName = firstName,
+  //     this.lastName = lastName
+  // }
+  
+  //veya
+  id:number;
+  firstName:string;
+  lastName:string;
+
+  constructor(id:number, firstName:string, lastName:string){
+      this.id = id,
+      this.firstName = firstName,
+      this.lastName = lastName
+  }
+
+  getFullName(){
+      //return this.firstName +' '+this.lastName;
+      return `${this.firstName} ${this.lastName}`
+  }
+}
+
+let kisiBilgim= new Person(1,"Gözde","Cankı");
 console.log(kisiBilgim);
 console.log(kisiBilgim.getFullName());
+
+*/
+//access modifiers => public , protected, private
+/* class Person {
+
+   public id:number;// modifier belirtmez isek public tir. default değer publictir
+   private firstName:string;// class içinde erişilir. dışarıdan erişim yok.
+   protected lastName:string;// class içinde erişilir. dışarıdan erişim yok. bir classtan diğer bir classı inherit(extend) ettiğimizde
+                             // , diğer classtan protected olan alana erişebiliyoruz.(yani subclasslarda erişim var)
+
+    constructor(id:number, firstName:string, lastName:string){
+        this.id = id,
+        this.firstName = firstName,
+        this.lastName = lastName
+    }
+
+    getFullName(){
+    
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+
+let kisiBilgim= new Person(23,"Gözde","Cankı");
+
+console.log(kisiBilgim.id);//public iken erişimde sorun yok
+//console.log(kisiBilgim.firstName);// hata verir. private değerlere ancak class içinde erişilir.
+//console.log(kisiBilgim.lastName);// hata verir. protected değerlere ancak class içinde veya inherit edilmiş olan subclasslarda erişilir. */
+//readonly 
+/* class Person {
+
+   readonly id:number;// bu keyword varken bu alanın değerini değiştiremiyoruz
+   firstName:string;
+   lastName:string;
+                         
+
+   constructor(id:number, firstName:string, lastName:string){
+       this.id = id,
+       this.firstName = firstName,
+       this.lastName = lastName
+   }
+
+   getFullName(){
+   
+       return `${this.firstName} ${this.lastName}`
+   }
+}
+
+let kisiBilgim= new Person(23,"Gözde","Cankı");
+console.log(kisiBilgim.id); */
+/* kisiBilgim.id=10;// hata verir
+console.log(kisiBilgim.id);
+ */
+//inheritance
+/* class Person {
+
+    id:number;
+    firstName:string;
+    lastName:string;
+                          
+    constructor(id:number, firstName:string, lastName:string){
+        this.id = id,
+        this.firstName = firstName,
+        this.lastName = lastName
+    }
+
+    getFullName(){
+    
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+
+
+class Employee extends Person{
+    constructor ( id:number, firstName:string,lastName:string){
+        super(id, firstName,lastName);//parentin constructor ını çağırıyoruz
+     }
+
+}
+
+let employee = new Employee(29,"Gözde","Cankı");
+console.log(employee.id);
+console.log(employee.getFullName()); */
+//static methods
+/*
+class Circle{
+  static pi:number =3.14; //classtan nesne üretmeden erişebiliriz. Sınıf adını kullanarak erişim sağlarız.
+         pi=3;//static olmadığı için nesne yaratıp o nesne üzerinden erişim sağlıyoruz.
+
+         constructor(){
+            Circle.pi++;//her new instance ı aldığında 1 arttır
+            this.pi++;//
+         }
+
+  static hesapla(yaricap:number){
+    return this.pi * yaricap * yaricap;
+  }
+
+}
+
+let objem = new Circle();
+console.log("static değil: " + objem.pi);//4 yazacak
+
+let objem2 = new Circle();
+console.log("static değil2: " +objem2.pi);//4 yazacak
+
+//4 yazma sebebi, static olmadığı için her new instance oluştururken pi=3 üzerinden işlem yapar. her nesne için sabittir. ama static değer sınıfın her nesnesi için aynıdır.
+//bu sebeple aşağıdaki durum oluşur
+console.log("static : " +Circle.pi);//2 tane new ile nesnesini oluşturduk bu yüzden 3.14+1+1=5.14 olur
+console.log("static hesapla: " +Circle.hesapla(5)); */
+//abstract class--  kendi başına new ile nesnesini oluşturamıyoruz. ama extend edebiliriz.
+/* abstract class Department{
+
+    constructor(public name: string){}
+
+    printName(): void {// abstract classlarda abstract method tanımlamak zorunda değiliz.
+        console.log("department name: " + this.name);
+    }
+
+    abstract printMeeting(): void; // abstract method tanımlaması yaptığımızda extend ettiğimiz sınıfta implement edilmesi gerekmektedir zorunludur. aksi durumda ide de hata alırız.
+
+}
+
+class AccountingDepartment extends Department{
+    constructor(){
+        super("Accounting and Auditing");
+    }
+
+    printMeeting(): void {//abstract method implement edildi
+        console.log("the accounting department meets each Monday at 10 am.");
+    }
+
+    generateReports(): void{
+         console.log("Generating accounting reports...");
+    }
+
+}
+
+//let dep = new Department(); hatalı. abstract classın new  instance oluşturamayız.
+let dep : Department;//fakat abstract type a referans edebiliriz
+dep = new AccountingDepartment();
+dep.printName();
+dep.printMeeting();
+//dep.generateReports(); bu method abstract class da tanımı değil
+
+let department = new AccountingDepartment();
+department.printName();
+department.printMeeting();
+department.generateReports();*/
+//interface 
+//interfacesiz 
+/* function getFullName(person :{ // person bu şekilde tip vermeden de kullanırsak tipler implicitly any olarak kabul görür. bu şekilde tip vererek tanımladığımızda tip kontrolü sağlar. ama kod biraz karışık olur
+    //10 tane property olursa onları da buraya eklememiz gerekir
+    firstName:string,
+    lastName: string
+}) {
+    return `${person.firstName} ${person.lastName}`;
+}
+
+let person={
+    firstName: 'Gözde',
+    lastName:'Cankı'
+}
+console.log(getFullName(person));
+  
+  //interfaceli
+interface Person {
+    firstName:string,
+    lastName: string
+}
+function getFullName2(person: Person){
+      return `${person.firstName} ${person.lastName}`;
+}
+
+let person2={
+    firstName: 'Gözde',
+    lastName:'Cankı'
+}
+console.log(getFullName(person2));
+
+let person3={
+    firstName: 'Gözde',
+    lastName:'Cankı',
+    age:29// interface de olmayan bir alan tanımlayabiliriz
+}
+console.log(getFullName(person3)); */
+//interface optional parameters
+/* interface Person {
+    firstName:string,
+    lastName: string,
+    middleName?: string //optional tanımlama ?
+}
+function getFullName(person: Person){
+    if(person.middleName){
+        return `${person.firstName} ${person.lastName} ${person.middleName} `;
+    }
+      return `${person.firstName} ${person.lastName}`;
+}
+
+let person={
+    firstName: 'Gözde',
+    lastName:'Cankı',
+  //  middleName: 'Meriç', bu satırı yazmasak da olur çünkü optional
+    age: 33
+}
+console.log(getFullName(person));
+
+//readonly
+
+interface Person2 {
+   readonly firstName:string,
+    lastName: string,
+}
+let person2 : Person2={
+    firstName: 'Gözde',// ilk kez tanımı yapıyoruz. daha sonra değişiklik yapmak istediğimizde yapamıyoruz
+    lastName:'Cankı',
+   // age:23 // bir de person2 yi bu şekilde oluşturduğumuzda farklı bir alan tanımlayamıyoruz.
+}
+//person2.firstName="Gizem";// readonly tanımlama yapıldığı için değişiklik yapamayız
+console.log(getFullName(person2));
+*/
+//interface declaring function type
+/* interface StringFormat{
+    (str: string, isUpper:boolean ):string
+}
+
+let format : StringFormat;
+format = function(str:string,isUpper:boolean){
+    return isUpper ? str.toLocaleUpperCase() : str.toLocaleLowerCase();
+}
+console.log(format("Gözde Cankı", true));
+*/
+//interface extend etme 
+/* interface IPerson{
+    name:string;
+    gender:string;
+}
+
+interface IEmployee extends IPerson{// Ipersonda tanımlanan fieldlara sahip olur. bu alanları tekrar tekrar yazmamıza gerek kalmamış oluyor
+    empNumber :number;
+}
+interface IWorker extends IPerson{
+    empDepartment:string;
+}
+
+
+let employee : IEmployee= {//tüm alanları vermek zorundayız
+    empNumber:1,
+    gender: "Female",
+    name:"Gözde"
+}
+console.log(employee);
+
+
+let employeeDepartment : IWorker= {//tüm alanları vermek zorundayız
+    empDepartment:"Software",
+    gender: "Female",
+    name:"Gözde"
+}
+console.log(employeeDepartment); */
+//bir class a interface implement etme
+/*
+ interface IPerson{
+    name:string;
+    gender:string;
+}
+
+class Employee implements IPerson{// interface içinde tanımlanmış olan tüm propertyleri yazmak zorundayız
+    empNumber: number;
+    name: string;//Iperson field
+    gender: string;//Iperson field
+
+    constructor(empNumber:number, name:string, gender:string ){
+        this.empNumber=empNumber;
+        this.name=name;
+        this.gender=gender;
+    }
+}
+
+let employee = new Employee(5,"Gözde","Female");
+console.log(employee); */
+//type intersection
+/* let varName : number | string // bu şekilde veya kullanıyorduk tanımlama yaparken
+
+interface BusinessPartner{
+    name: string;
+    credit: number;
+   
+}
+interface Identity{
+    name:string;
+    id:number;
+}
+
+interface Contact{
+    email:string;
+    phone:string;
+}
+
+type Employee = Identity & Contact; // employee hem Identity hem de Contact ın fieldlarını içeren yeni bir type oluşturmuş olduk
+let kisi : Employee={//bu lanlardan herhangibirini tanımlamazsak hata alırız. çünkü type ı &(ve) ile oluşturuyoruz.
+id:54,
+name:"Gözde Cankı",
+email:"gozde@gmail.com",
+phone:"056623645"
+}
+console.log(kisi);
+
+type Customer = BusinessPartner & Contact;
+let musteri :Customer={
+    credit:456,
+    email:"hdfjd@hotmail.com",
+    name:"Gözde Can",
+    phone:"05645646265"
+
+}
+console.log(musteri); */
+//type guard
+/* type tip = string | number;
+
+function add(a: tip, b: tip) {
+    if(typeof a === 'number' && typeof b ==='number'){
+        return a+ b;
+    }
+    if(typeof a === 'string' && typeof b ==='string'){
+        return a.concat(b);
+    }
+
+    throw new Error('Lütfen doğru formatta data gönderin');
+}
+
+console.log(add(5,6));
+console.log(add("Gözde"," Cankı"));
+console.log(add("Gözde",6)); */
+//instanceof
+/* class Customer{
+    isCreditAllowed(): boolean{
+        //...
+        return true;
+    }
+}
+class Supplier{
+     isInShortList(): boolean{
+        //...
+        return true;
+    }
+}
+
+type BusinessPartner = Customer | Supplier;
+
+function signContract(partner: BusinessPartner) : string {
+    let message : string ='';
+    if(partner instanceof Customer)
+        {
+        message = partner.isCreditAllowed() ? 'Sign a new contract with the customer' :'Credit issue';
+   
+    }
+      if(partner instanceof Supplier)
+        {
+        message = partner.isInShortList() ? 'Sign a new contract with the supplier' :'Need to evaluate further';
+       
+    }
+      return message;
+}
+
+let customer = new Customer();
+
+console.log(signContract(customer)); */
+//generics
+/*   //generic olmadan
+function getRandomNumber(items: number[]): number {
+   let randomIndex = Math.floor((Math.random() * items.length) );    //0-1 arasında sayı üretip item length ile çarpıp floor ile alt tam sayıya yuvarladık
+   return items[randomIndex];
+
+}
+console.log("ayrı ayrı function");
+let numbers=[1,54,65,7,8];
+console.log(getRandomNumber(numbers));
+
+function getRandomString(items: string[]): string {
+   let randomIndex = Math.floor((Math.random() * items.length) );    //0-1 arasında sayı üretip item length ile çarpıp floor ile alt tam sayıya yuvarladık
+   return items[randomIndex];
+}
+let adlarim=["can","gözde","ayşe"];
+console.log(getRandomString(adlarim));
+
+   //any ile --> type safe değildir
+function getRandomElement(items: any[]): any {
+   let randomIndex = Math.floor((Math.random() * items.length) );    //0-1 arasında sayı üretip item length ile çarpıp floor ile alt tam sayıya yuvarladık
+   return items[randomIndex];
+
+}
+console.log("any ile");
+console.log(getRandomElement(numbers));
+console.log(getRandomElement(adlarim));
+
+   //generic --> automatic type inference
+function getRandomElement2<T>(items: T[]): T {
+   let randomIndex = Math.floor((Math.random() * items.length) );    //0-1 arasında sayı üretip item length ile çarpıp floor ile alt tam sayıya yuvarladık
+   return items[randomIndex];
+
+}
+console.log("Generic ile");
+console.log(getRandomElement2(numbers));
+console.log(getRandomElement2(adlarim));
+
+//tip force edebiliriz
+console.log(getRandomElement2<number>(numbers));
+console.log(getRandomElement2<string>(adlarim));
+
+let degiskenlerim=[true, false, true];
+console.log(getRandomElement2(degiskenlerim));
+console.log(getRandomElement2<boolean>(degiskenlerim)); */
+//generic constraints
+/*
+function merge<U, V>(obj1: U, obj2:V){
+    return {
+        ...obj1, //spread operatörü
+        ...obj2
+    }
+}
+
+let person2 = merge (// bu şekilde çalışır fakat konsolda sadece name:"Gözde" değerini görürüz bunun için merge fonksiyonuna extends keywordu ekliyoruz
+    {name:"Gözde"},
+  //  {age:29}
+  29
+)
+console.log(person2);
+
+
+function merge2<U extends object, V extends object>(obj1: U, obj2:V){
+    return {
+        ...obj1, //spread operatörü
+        ...obj2
+    }
+}
+
+let person = merge2 (
+    {name:"Gözde"},
+    {age:29}
+    //29 artık bu şekilde ide de hata alırız
+)
+console.log(person);
+ */
+//interfacelerde generic kullanımı
+/* interface Months <U,V>{
+   key: U,
+   value : V;
+}
+
+let month: Months<number,string> ={
+    key:1,
+    value:'January'
+}
+console.log(month); */
+//interface methodlarında generic
+/* interface Collection <T>{
+   add(o :T): void;
+   remove(o:T): void;
+}
+
+class List<T> implements Collection<T>{
+   private items: T[]= [];
+  
+   add(o:T):void{
+      this.items.push(o);
+      console.log(this.items);
+   }
+
+   remove(o:T): void{
+       let index =this.items.indexOf(o);
+       if(index >-1){
+        this.items.splice(index,1);
+       }
+   }
+
+
+}
+
+let list = new List<number>();
+
+for (let i = 0; i < 10; i++) {
+   list.add(i);
+}
+ */
+//class larda generic kullanımı
+var Stack = /** @class */ (function () {
+    function Stack(size) {
+        this.size = size;
+        this.elements = [];
+    }
+    Stack.prototype.isEmpty = function () {
+        return this.elements.length === 0;
+    };
+    Stack.prototype.isFull = function () {
+        return this.elements.length === this.size;
+    };
+    Stack.prototype.push = function (element) {
+        if (this.elements.length === this.size) {
+            throw new Error('the stack is overflow!');
+        }
+        this.elements.push(element);
+    };
+    Stack.prototype.pop = function () {
+        if (this.elements.length === 0) {
+            throw new Error('the stack is empty!');
+        }
+        return this.elements.pop();
+    };
+    return Stack;
+}());
+function randBetween(low, high) {
+    return Math.floor(Math.random() * (high - low + 1) + low);
+}
+var numbers = new Stack(5);
+while (!numbers.isFull()) { //dolana kadar push yap
+    var n = randBetween(1, 10);
+    console.log("push ".concat(n, " into the stack."));
+    numbers.push(n);
+}
+while (!numbers.isEmpty()) {
+    var n = numbers.pop();
+    console.log("pop ".concat(n, " into the stack."));
+}
